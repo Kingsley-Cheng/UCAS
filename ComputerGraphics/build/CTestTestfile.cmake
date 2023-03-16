88d1123a30ee4c50fb58de5c93a8ad3f005cf1fe
Kingsley-Cheng <chengzijie2021@outlook.com>
2023-03-16 21:04:36 +0800

    2023-03-16

--- ComputerGraphics/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/kingsleycheng/Documents/github/UCAS/ComputerGraphics/Homework0
-# Build directory: /Users/kingsleycheng/Documents/github/UCAS/ComputerGraphics/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
